c8f2415cbd7bcaee2ada47b71c36a6e15ef225ad
cameron <cameron.lengerich@gmail.com>
2016-01-31 19:43:52 -0500

    Removed multithreaded code  Only launches one instance of VLC for each channel

--- streampicker.py
+++ streampicker.py
@@ -1,20 +1,12 @@
-import os
 import json
+import os
 import tkinter as tk
 from io import BytesIO
 from tkinter import simpledialog, messagebox
-from threading import Thread
+import subprocess
 
 import requests
 from PIL import ImageTk, Image
-
-
-class Stream:
-    def __init__(self, display_name, preview_url, viewer_count):
-        self.display_name = display_name
-        self.preview_url = preview_url
-        self.viewer_count = viewer_count
-        self.preview_img = None
 
 
 class StreamPicker(tk.Frame):
@@ -25,7 +17,7 @@ class StreamPicker(tk.Frame):
         self.base_url = 'https://api.twitch.tv/kraken'
         self.oauth_token = 'zk5z17f88iqq347yn63rpbubtadumd'
         self.frames = []
-        self.all_streams = []
+        self.streams = []
 
         viewer_im = Image.open(os.path.dirname(__file__) + '/viewer.png')
         viewer_im.thumbnail((15, 15))
@@ -58,6 +50,8 @@ class StreamPicker(tk.Frame):
         if live_streams:
             streams = live_streams['streams']
             self.extract_live_streams(streams)
+        else:
+            tk.Label(root, text='No streams available').pack()
 
     def get_live_streams(self):
         r = requests.get('{}/streams/followed?oauth_token={}'.format(self.base_url, self.oauth_token))
@@ -65,46 +59,22 @@ class StreamPicker(tk.Frame):
 
         return live_streams
 
-    @staticmethod
-    def get_preview_img(stream):
-        response = requests.get(stream.preview_url)
-        print('got image for ' + stream.display_name)
-        preview_im = Image.open(BytesIO(response.content))
-        preview_im.thumbnail((250, 260))
-        stream.preview_img = ImageTk.PhotoImage(preview_im)
-
     def extract_live_streams(self, streams):
-        if not streams:
-            tk.Label(root, text='No streams available').pack()
-
         for stream in streams:
             display_name = stream['channel']['display_name']
             preview_url = stream['preview']['large']
             viewer_count = stream['viewers']
-            self.all_streams.append(Stream(display_name, preview_url, viewer_count))
+            response = requests.get(preview_url)
+            preview_im = Image.open(BytesIO(response.content))
+            preview_im.thumbnail((250, 260))
 
-        threads = []
-        for stream in self.all_streams:
-            t = Thread(target=self.get_preview_img(stream))
-            t.start()
-            threads.append(t)
+            preview_img = ImageTk.PhotoImage(preview_im)
 
-        for thread in threads:
-            thread.join()
-
-        for stream in self.all_streams:
             if self.column >= 3:
                 self.column = 0
                 self.row += 1
-            self.display_stream_option(stream.display_name, stream.viewer_count, stream.preview_img)
+            self.display_stream_option(display_name, viewer_count, preview_img)
             self.column += 1
-
-        self.all_streams.clear()
-        threads.clear()
-
-
-
-
 
     def display_stream_option(self, channel, viewers, img):
         """Add stream to grid
@@ -141,31 +111,21 @@ class StreamPicker(tk.Frame):
         else:
             url = 'twitch.tv/' + channel
 
-        command = 'screen -d -m -S {} /usr/local/bin/livestreamer {} best'.format(channel, url)
+        count = subprocess.check_output('screen -ls | grep {} | wc -l'.format(channel), shell=True)
+        if int(count) >= 1:  # another instance running -- don't open
+            return
+
+        command = 'screen -d -m -S {} livestreamer {} best'.format(channel, url)
         try:
             os.system(command)
         except Exception as e:
             messagebox.showinfo('StreamPicker', e)
 
     def show_open_stream_dialog(self, service):
-        # open_stream_dialog = tk.Toplevel(root, bg='gray15')
-        # open_stream_dialog.geometry('250x50+300+150')
-        # label = tk.Label(open_stream_dialog, text=service + '.tv/', fg='bisque', bg='gray15')
-        # entry = tk.Entry(open_stream_dialog, fg='bisque', bg='gray15')
-        # button = tk.Button(open_stream_dialog, text='Open', bg='gray15',
-        #                    command=lambda: self.open_nontwitch_stream(service, entry.get(), open_stream_dialog))
-        # label.grid(row=1, column=0)
-        # entry.grid(row=1, column=1)
-        # button.grid(row=2, column=1)
 
         channel = simpledialog.askstring('StreamPicker', service + '.tv/')
         url = '{}.tv/{}'.format(service, channel)
         self.open_stream(url)
-
-    # def open_nontwitch_stream(self, service, channel, open_stream_dialog):
-    #     url = '{}.tv/{}'.format(service, channel)
-    #     open_stream_dialog.forget()
-    #     self.open_stream(url)
 
     def refresh(self):
         for frame in self.frames:
